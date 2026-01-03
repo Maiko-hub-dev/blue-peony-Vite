4f1a9d6054580f5a05092870c4f7faabf25d9f2a
Mai <morn43n4mai@gmail.com>
2026-01-03 10:44:50 +0900

    TOPページのSP版とローディングの修正

--- src/js/script.js
+++ src/js/script.js
@@ -92,8 +92,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 //logoの表示
-$(window).on('load',function(){
-  $("#splash").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
-  $("#splash_logo").delay(1200).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
+$(window).on('load', function () {
+  $("#splash_logo").delay(1200).fadeOut('slow');
+
+  $("#splash").delay(1500).fadeOut('slow', function () {
+    // ローディング終了
+    document.body.classList.remove('is-loading');
+  });
 });
 
